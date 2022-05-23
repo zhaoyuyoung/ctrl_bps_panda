0e1248cdf1d25aa9a19536c0080a583817be0e5f
Mikolaj Kowalik <mxk@illinois.edu>
2022-05-23 15:18:25 -0400

    Add missing __all__ statement

--- python/lsst/ctrl/bps/panda/panda_service.py
+++ python/lsst/ctrl/bps/panda/panda_service.py
@@ -19,6 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+__all__ = ["PanDAService", "PandaBpsWmsWorkflow"]
+
+
 import binascii
 import concurrent.futures
 import logging
