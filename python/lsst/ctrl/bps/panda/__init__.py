f184a5a38758d443def3973f9cf5199fa4cbdfe3
Tim Jenness <tjenness@lsst.org>
2022-06-23 10:16:24 -0700

    Import a version

--- python/lsst/ctrl/bps/panda/__init__.py
+++ python/lsst/ctrl/bps/panda/__init__.py
@@ -19,8 +19,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import pkgutil
-
-__path__ = pkgutil.extend_path(__path__, __name__)
-
 from .panda_service import *
+from .version import *
